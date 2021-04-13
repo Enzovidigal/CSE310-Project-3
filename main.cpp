0b54fb4613289e7b7082b023d61ae4c61d20385b
Enzovidigal <enzovidigal@gmail.com>
2021-04-13 12:33:04 -0300

    reading command line arguments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+//#include "defn.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
 
+int main(int argc, char* argv[]) {
+    char* graph = argv[1];      // gets name of text file
+    char* direction = argv[2];  // gets direction of the graph
+    int n;                      // number of vertices
+    int m;                      // number of edges
+    char first_ln[256];         // first line of the graph text file
+
+    //read text file
+    FILE* stream = fopen(graph, "r");
+
+    if (stream != NULL) {
+        // gets values for n and m
+        fgets(first_ln, sizeof(first_ln), stream);
+        char* token = strtok(first_ln, " ");
+        n = atoi(token);
+
+        token = strtok(NULL, " ");
+        m = atoi(token);
+
+        fclose(stream);
+    }
     return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 dijkstra: main.o
-	g++ main.o-std=c++11 -o dijkstra
+	g++ main.o -std=c++11 -o dijkstra
 
 main.o: main.cpp
 	g++ main.cpp -c
